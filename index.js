50cb1bc1479a6323aed4e9a486b323e4c0c1cecf
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-03-19 20:32:40 +0530

    remove static rendering files

--- index.js
+++ index.js
@@ -2,27 +2,21 @@ const express = require('express');
 const socket = require('socket.io');
 const app = express();
 
-app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public/'));
 
 const port = process.env.PORT || 3100;
 
-const server = app.listen(port, () => {
-    console.log(`Running at port ${port}`);
-})
-
-const io = socket(server);
-
-const id1 = Math.floor(Math.random()*10000000)
-
 app.get('/', (req, res) => {
-    res.redirect(`/${id1}`);
-})
-
-app.get('/:id', (req, res) => {
-    res.render('index.ejs')
+    // send built react app on this route
+    res.send('hi');
 });
 
+const server = app.listen(port, () => {
+    console.log(`Running at port ${port}`);
+});
+const io = socket(server);
+
+// Lobbies for clients
 const lobbies = {};
 
 io.on('connect', (socket) => {
@@ -60,4 +54,4 @@ io.on('connect', (socket) => {
             });
         }
     })
-})
+});
